37016dc1c9175775cf2d427a2e46aabdb8c495fa
Nafies Luthfi <nafiesl@gmail.com>
2018-03-24 08:21:03 +0800

    Setup Browserkit test suite with sqlite in memory

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\User;
+use Laravel\BrowserKitTesting\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected $baseUrl = 'http://localhost';
+
     use CreatesApplication;
+
+    public function loginAsAdmin()
+    {
+        $admin = factory(User::class)->create();
+        $this->actingAs($admin);
+
+        return $admin;
+    }
 }
